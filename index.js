a9d4639b314dd4012c94229b746bafd599a65e82
Mark Howell <MHowellJr16@gmail.com>
2025-10-15 14:39:36 -0400

    Oct. 15__Final Update

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-
+const userListEl = document.querySelector(".user-list")
 
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
-  console.log(usersData); 
-  const userListEl = document.querySelector(".user-list")
   userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
   }
 
@@ -21,7 +19,7 @@ async function main() {
   function userHTML(user) {
    return ` <div class="user-card" onclick = "showUserPosts(${user.id})">
               <div class="user-card__container">
-                <h3>${user.name}</h4>
+                <h4>${user.name}</h4>
                   <p><b>Email:</b> ${user.email}</p>
                   <p><b>Phone:</b> ${user.phone}</p>
                   <p><b>Website:</b> <a href="https://${user.website}" target="_blank">

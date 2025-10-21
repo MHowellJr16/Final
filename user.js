664b2e7f58208f28aeba28350afc207184ce1395
Mark Howell <MHowellJr16@gmail.com>
2025-10-21 12:58:28 -0400

    Oct. 21__FInal Update

--- user.js
+++ user.js
@@ -1,9 +1,20 @@
-
+const postsListEl = document.querySelector(".posts-list");
 async function main() {
   const id = localStorage.getItem("id");
   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
-  const postsData = posts.json();
+  const postsData = await posts.json();
   console.log(postsData);
+
+  postsListEl.innerHTML = postsData.map (post => `
+     <div class="post">
+      <div class="post__title">
+        Post Title
+      </div>
+      <p class="post__body">
+        Post Body
+      </p>
+    </div>`
+  ).join("");
 }
 
 main();
